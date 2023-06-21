ae07ebaf19f31540e0e3f47bae1417a1a21777f7
Abelmoreno97 <abelyamilmoreno@gmail.com>
2023-06-21 12:38:31 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express"
+import config from './config'
+import productRoutes from './routes/product.routes'
+
+const app = express()
+
+
+app.set('port', config.port)
+
+app.use(productRoutes)
+
+export default app
---
+++ src/database/connection.js
@@ -0,0 +1,22 @@
+import sql from 'mssql'
+
+const dbSettings = {
+    user: 'abel',
+    password: '123',
+    server:'localhost',
+    database: 'abeldb',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true,
+    },
+}
+
+export async function getConnection () {
+    try {
+        const pool = await sql.connect(dbSettings);
+        return pool;
+    } catch (error) {
+        console.log(error);
+    }
+
+}
