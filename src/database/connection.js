98fd1e079196ca1fc525898eb914f05dedd46fa5
Abelmoreno97 <abelyamilmoreno@gmail.com>
2023-06-21 16:01:10 -0300

    1234

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,22 +1,21 @@
-import sql from 'mssql'
+import sql from "mssql";
 
 const dbSettings = {
-    user: 'abel',
-    password: '123',
-    server:'localhost',
-    database: 'abeldb',
-    options: {
-        encrypt: true,
-        trustServerCertificate: true,
-    },
-}
+  user: "abel",
+  password: "123",
+  server: "localhost",
+  database: "abeldb",
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
 
-export async function getConnection () {
-    try {
-        const pool = await sql.connect(dbSettings);
-        return pool;
-    } catch (error) {
-        console.log(error);
-    }
-
+export async function getConnection() {
+  try {
+    const pool = await sql.connect(dbSettings);
+    return pool;
+  } catch (error) {
+    console.log(error);
+  }
 }
